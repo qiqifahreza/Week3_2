8895af983e57f994e1b5b5c4ad95e97ca8370ada
RifkyAlFareza <156492712+qiqifahreza@users.noreply.github.com>
2026-05-06 00:51:41 +0700

    Fix product price initialization and correct method call in Main class

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Produk p = new Produk("Laptop", 15000000, 10, "Elektronik");
+        Produk p = new Produk("Laptop", 15000.0, 10, "Elektronik");
         
         System.out.println("Nama Produk: " + p.getNama());
         System.out.println("Harga: " + p.getHarga());
-        p.getHarga(20000);
+        p.setHarga(20000.0);
+        System.out.println("Harga setelah diubah: " + p.getHarga());
 
     }
 }
