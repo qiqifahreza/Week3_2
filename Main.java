19d42fd321aa88e4baa711125ada47834ffe8ff5
RifkyAlFareza <156492712+qiqifahreza@users.noreply.github.com>
2026-05-06 01:17:19 +0700

    Fix product price initialization in Main class for correct value representation

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Elektronik elektronik1 = new Elektronik("Laptop", 15000000, 10, "Elektronik", 5);
+        Elektronik elektronik1 = new Elektronik("Laptop", 150000.0, 10, "Elektronik", 5);
         elektronik1.setGaransi(0);
         elektronik1.tampilInfo();
 
