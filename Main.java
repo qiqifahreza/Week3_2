dc9da8b47170b899532fdd822cce58b254b9c128
RifkyAlFareza <156492712+qiqifahreza@users.noreply.github.com>
2026-05-06 00:43:24 +0700

    Refactor Main and Produk classes to enhance product information display

--- Main.java
+++ Main.java
@@ -3,7 +3,10 @@ import base.Produk;
 public class Main {
     public static void main(String[] args) {
         Produk p = new Produk("Laptop", 15000000, 10, "Elektronik");
-        p.tampiInfo();
         
+        System.out.println("Nama Produk: " + p.getNama());
+        System.out.println("Harga: " + p.getHarga());
+        p.getHarga(20000);
+
     }
 }
--- base/Produk.java
+++ base/Produk.java
@@ -12,6 +12,22 @@ public class Produk {
         this.stok = stok;
         this.kategori = kategori;
     }
+    public String getNama() {
+        return nama;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+    public int getStok() {
+        return stok;
+    }
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+    public void setStok(int stok) {
+        this.stok = stok;
+    }
 
     public void tampiInfo() {
         System.out.println("Nama Produk: " + nama);
