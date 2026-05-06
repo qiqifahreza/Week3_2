065fd7b69a0f7ef9367a266efc780d47b7937040
qiqifahreza <bungbeng002@gmail.com>
2026-05-06 10:45:32 +0700

    Fix constructor parameters in Elektronik class to include brand name

--- Elektronik.java
+++ Elektronik.java
@@ -3,7 +3,7 @@ public class Elektronik extends Produk {
     private String merek;
     private static final double PPN = 0.11;
 
-    public Elektronik(String nama, double harga, int stok, int garansiTahun) {
+    public Elektronik(String nama, String merek, double harga,int stok, int garansiTahun) {
         super(nama, harga, stok);
         this.merek = merek;
         this.garansiTahun = garansiTahun;
--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Elektronik laptop = new Elektronik("laptop Asus",850000.0,10 ,"Asus",2 );
-        Elektronik Hp = new Elektronik("Hp Samsung", 3200000.0, 25, "Samsung", 1);
+        Elektronik laptop = new Elektronik("laptop Asus", 850000, 20,  );
+        Elektronik Hp = new Elektronik("Hp Samsung", 3200000, 25, 1);
 
         Makanan indomie = new Makanan("Indomie Goreng", 3500.0, 100, "Mie Instant", "2026-12-01");
 
