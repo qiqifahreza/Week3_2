05f719b7a833f2be9b75f52b6df8419df7063836
qiqifahreza <bungbeng002@gmail.com>
2026-05-06 12:59:26 +0700

    Add transaction processing for purchased items in Main class

--- Main.java
+++ Main.java
@@ -41,7 +41,11 @@ public class Main {
         System.out.println("Diskon: Rp " + roti.hitungDiskon(5, 15));
         System.out.println("Total: Rp " + (roti.hitungTootalHarga(5) - roti.hitungDiskon(5, 15)));
 
-        
+        System.out.println("\n==== Transaksi ====");
+        laptop.beli(1);
+        Hp.beli(2);
+        indomie.beli(10);
+        roti.beli(5);
     }
     
 }
