413db8b7d4781062366b211aba2b97bcfb6f1530
qiqifahreza <bungbeng002@gmail.com>
2026-05-06 10:29:30 +0700

    Refactor Elektronik class constructor to include garansiTahun and remove kategori parameter

--- Elektronik.java
+++ Elektronik.java
@@ -1,8 +1,12 @@
 public class Elektronik extends Produk {
-    private int garansi;
-    public Elektronik(String nama, double harga, int stok, String kategori, int garansi) {
-        super(nama, harga, stok, kategori);
-        this.garansi = garansi;
+    private int garansiTahun;
+    private String merek;
+    private static final double PPN = 0.11;
+
+    public Elektronik(String nama, double harga, int stok, int garansiTahun) {
+        super(nama, harga, stok);
+        this.merek = merek;
+        this.garansiTahun = garansiTahun;
     }
     @Override
     public void tampilInfo() {
