a9dffc40dfe9306d5537c7b5afd453cbd9c198be
qiqifahreza <bungbeng002@gmail.com>
2026-05-06 10:53:27 +0700

    Fix Elektronik constructor parameters in Main class to include brand name

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Elektronik laptop = new Elektronik("laptop Asus", 850000, 20,  );
-        Elektronik Hp = new Elektronik("Hp Samsung", 3200000, 25, 1);
+        Elektronik laptop = new Elektronik("Laptop Asus", "Asus", 850000, 10, 2);
+        Elektronik Hp = new Elektronik("Hp Samsung", "Samsung", 3200000, 25, 1);
 
         Makanan indomie = new Makanan("Indomie Goreng", 3500.0, 100, "Mie Instant", "2026-12-01");
-
+        Makanan roti = new Makanan("Roti Tawar", 3500.0, 50, "Roti", "2026-04-30");
     }
 }
